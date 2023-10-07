c592d4068de01e7d955038113d074a975ca1bd81
eb94596 <50734136+eb94596@users.noreply.github.com>
2023-10-07 15:18:54 +0900

    Update install.sh

--- helloworld-rs/extensions/install.sh
+++ helloworld-rs/extensions/install.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-echo "---injected_dir---"
 injected_dir=$1
-echo ${injected_dir}
-echo "---injected_dir end"
-
-echo "---source /usr/local/s2i/install-common.sh---"
 source /usr/local/s2i/install-common.sh
-echo "---source /usr/local/s2i/install-common.sh end---"
-
-echo "---install_modules ${injected_dir}/modules---"
 install_modules ${injected_dir}/modules
-echo "---install_modules ${injected_dir}/modules end---"
-
-echo "---configure_drivers ${injected_dir}/drivers.env---"
 configure_drivers ${injected_dir}/drivers.env
-echo "---configure_drivers ${injected_dir}/drivers.env end---"
